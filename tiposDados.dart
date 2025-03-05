c523df2bc099fed9a6d7e7d99eaf37c8fe40e9f5
Evelyn Becker <beckernyleve@gmail.com>
2025-03-05 11:04:54 -0300

    alterando variaveis

--- tiposDados.dart
+++ tiposDados.dart
@@ -6,6 +6,13 @@ void main(){
 *int - numeros inteiros sem casas decimais
 *double - numeros com ponto flutuante e casas decimais
  */
-int idade = 18;
+int idade = 19;
+double altura = 1.70;
+
+String pessoa = "Maria";
+double salario = 49.560;
+
+int ano = 2025;
+print(idade);
 
 }
